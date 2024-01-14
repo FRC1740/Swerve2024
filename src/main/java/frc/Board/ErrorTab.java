1c189ce5d18f023d1f584108005ac4b7a39fea0e
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-14 17:36:35 -0500

    ErrorTab cleanup

--- src/main/java/frc/Board/ErrorTab.java
+++ src/main/java/frc/Board/ErrorTab.java
@@ -4,6 +4,7 @@
 package frc.Board;
 
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
@@ -43,15 +44,21 @@ public class ErrorTab {
     if(!UsedNames.contains(nameOfCaller)){
       UsedNames += nameOfCaller;
       m_nte_Errors = m_sbt_Errors.add(nameOfCaller, newError).getEntry();
+      DriverStation.reportError(newError, false);
     }
   }
-  public boolean errorExists(String nameOfCaller, String newError) {
+  public boolean errorExists(String nameOfCaller, String newError) { // this is seperate because I want it to be able to be called
     if(CurrentErrors.contains(newError)){
       return true;
-    }else{
+    }
+    return false;
+  }
+  public boolean handleError(String nameOfCaller, String newError){
+    boolean errorExists = errorExists(nameOfCaller, newError);
+    if(!errorExists){ // meaning it hasn't been logged yet
       CurrentErrors += newError;
       logError(nameOfCaller, newError);
     }
-    return false;
+    return errorExists;
   }
 }
