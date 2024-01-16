5b363920c06fbdb8911b967aee4c523f2258a18c
Ronan <roal1878@gmail.com>
2024-01-16 18:45:53 -0500

    Changed getRotation2d to use the gyro's own getRotation2d method

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -347,7 +347,7 @@ public class DriveSubsystem extends SubsystemBase {
 
 //Gets gyro in form of Rotation2d
   public Rotation2d getRotation2d(){ // this is the reversed angle and should be used to get the reversed robot angle
-    return Rotation2d.fromDegrees(m_gyro.getAngle() * ((GyroConstants.kGyroReversed) ? -1 : 1));
+    return m_gyro.getRotation2d();
   }
 
   /**
