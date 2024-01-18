37367af058ea5aef0f93e0d5c012397f2f7f9645
Robro1878 <roal1878@gmail.com>
2024-01-17 19:41:34 -0500

    Added data logging to onthefly pathing, should be useful for debugging it

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,7 +148,7 @@ public class RobotContainer {
       
     m_driverController.y()
       .whileTrue(
-        OnTheFlyPathing.getOnTheFlyPath(0, 0)
+        new OnTheFlyPathing().getOnTheFlyPath(0, 0)
       );
 
     m_driverController.rightStick()
