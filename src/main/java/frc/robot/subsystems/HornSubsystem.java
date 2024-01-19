04ab58fc70bf3e8f3eeb9dfe5b1b9cd27fe74bc8
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-19 16:57:34 -0500

    Horn testing

--- src/main/java/frc/robot/subsystems/HornSubsystem.java
+++ src/main/java/frc/robot/subsystems/HornSubsystem.java
@@ -8,37 +8,17 @@ import frc.robot.constants.HornConstants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkBase.ControlType;
-
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class HornSubsystem extends SubsystemBase {
   private final CANSparkMax m_HornMotorLeader = new CANSparkMax(HornConstants.kHornLeaderMotorPort, CANSparkMax.MotorType.kBrushless);
   private final CANSparkMax m_HornMotorFollower = new CANSparkMax(HornConstants.kHornFollowerMotorPort, CANSparkMax.MotorType.kBrushless);
   private final RelativeEncoder m_HornMotorEncoder;
-  private SparkPIDController m_PidController;
 
-
-  NetworkTable HornTable = NetworkTableInstance.getDefault().getTable("Horn");
-
-  //PID network tables publishers
-  DoublePublisher P_Pub = HornTable.getDoubleTopic("P").publish();
-  DoublePublisher I_Pub = HornTable.getDoubleTopic("I").publish();
-  DoublePublisher D_Pub = HornTable.getDoubleTopic("D").publish();
-
-  //Flywheel Velocity publishers (Meters/second)
-  DoublePublisher Velocity_Pub = HornTable.getDoubleTopic("Velocity meters/sec").publish();
   /** Creates a new GroundIntake. */
   public HornSubsystem() {
     m_HornMotorFollower.follow(m_HornMotorLeader, false); //invert might have to be changed to true
     m_HornMotorEncoder = m_HornMotorLeader.getEncoder();
-    m_PidController = m_HornMotorLeader.getPIDController();
-
-    setPID(HornConstants.kP, HornConstants.kI, HornConstants.kD);
 
     burnFlash();
   }
@@ -56,11 +36,11 @@ public class HornSubsystem extends SubsystemBase {
     }
 
   public void setHornSpeed(double speed) {
-    m_PidController.setReference(speed, ControlType.kVelocity);
+    m_HornMotorLeader.set(speed);
   }
 
   public void stopHorn() {
-    m_PidController.setReference(0, ControlType.kVelocity);
+    m_HornMotorLeader.set(0.0);
   }
 
   @Override
@@ -69,30 +49,10 @@ public class HornSubsystem extends SubsystemBase {
     // Report the actual speed to the shuffleboard
     // m_GroundIntakeTab.setIntakeSpeed(getIntakeVelocity());
     // m_intakeSetSpeed = m_GroundIntakeTab.getIntakeSetSpeed();
-
-    publishPIDvalues();
-    publishVelocity();
-
   }
 
   private void burnFlash() {
     m_HornMotorLeader.burnFlash();
     m_HornMotorFollower.burnFlash();
-  }
-
-  private void publishPIDvalues(){
-    P_Pub.set(m_PidController.getP());
-    I_Pub.set(m_PidController.getI());
-    D_Pub.set(m_PidController.getD());
-  }
-
-  private void setPID(double P, double I, double D){
-    m_PidController.setP(P);
-    m_PidController.setI(I);
-    m_PidController.setD(D);
-  }
-
-  private void publishVelocity(){
-    Velocity_Pub.set(m_HornMotorEncoder.getVelocity());
   }
 }
