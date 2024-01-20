56bc2419fa26ba8b4d481d26200f9cc95d4f04de
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-19 20:38:19 -0500

    Log rpm on horn

---
+++ src/main/java/frc/Board/HornTab.java
@@ -0,0 +1,46 @@
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.Board;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class HornTab {
+  Shuffleboard m_sb;
+
+  ShuffleboardTab m_sbt_Horn;
+
+  GenericEntry m_nte_HornSpeed;
+
+
+  private static HornTab instance = null;
+
+  private HornTab() {
+    initShuffleboardTab();
+  }
+
+  public static HornTab getInstance() {
+    if(instance == null) {
+      instance = new HornTab();
+    }
+    return instance;
+  }
+
+  private void initShuffleboardTab() {
+    // Create and get reference to the tab
+    m_sbt_Horn = Shuffleboard.getTab("DriveTrain");
+
+    m_nte_HornSpeed = m_sbt_Horn.addPersistent("IMU Pitch", 0.0)
+      .withSize(2,1).withPosition(4,1).getEntry();
+  }
+  public Double getHornSpeed() {
+    return m_nte_HornSpeed.getDouble(0.0);
+  }
+
+  public void setHornSpeed(Double value) {
+    m_nte_HornSpeed.setDouble(value);
+  }
+}
--- src/main/java/frc/robot/subsystems/HornSubsystem.java
+++ src/main/java/frc/robot/subsystems/HornSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import frc.Board.HornTab;
 import frc.robot.constants.HornConstants;
 
 import com.revrobotics.CANSparkMax;
@@ -11,10 +12,12 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class HornSubsystem extends SubsystemBase {
-  private final CANSparkMax m_HornRightMotor = new CANSparkMax(HornConstants.kHornLeaderMotorPort, CANSparkMax.MotorType.kBrushless);
-  private final CANSparkMax m_HornLeftMotor = new CANSparkMax(HornConstants.kHornFollowerMotorPort, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_HornRightMotor = new CANSparkMax(HornConstants.kHornRightMotorPort, CANSparkMax.MotorType.kBrushless);
+  private final CANSparkMax m_HornLeftMotor = new CANSparkMax(HornConstants.kHornLeftMotorPort, CANSparkMax.MotorType.kBrushless);
   private final RelativeEncoder m_HornLeftEncoder;
   private final RelativeEncoder m_HornRightEncoder;
+  
+  HornTab m_HornTab = HornTab.getInstance();
 
   /** Creates a new GroundIntake. */
   public HornSubsystem() {
@@ -33,9 +36,9 @@ public class HornSubsystem extends SubsystemBase {
     setHornSpeed(-speed);
   }
 
-  public double getHornvelocity() {
-    return (m_HornRightEncoder.getVelocity() + m_HornLeftEncoder.getVelocity()) / 2; 
-    }
+  public double getHornVelocity() {
+    return (m_HornRightEncoder.getVelocity() + m_HornLeftEncoder.getVelocity()) / 2;
+  }
 
   public void setHornSpeed(double speed) {
     m_HornRightMotor.set(speed);
@@ -49,11 +52,7 @@ public class HornSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // m_DriveTab.setIMU_ZAngle(10);
-    // This method will be called once per scheduler run
-    // Report the actual speed to the shuffleboard
-    // m_GroundIntakeTab.setIntakeSpeed(getIntakeVelocity());
-    // m_intakeSetSpeed = m_GroundIntakeTab.getIntakeSetSpeed();
+    m_HornTab.setHornSpeed(getHornVelocity());
   }
 
   private void burnFlash() {
