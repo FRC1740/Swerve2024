ff7fb84768a9e8e0ee431841dae61947b8f75a41
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-19 20:43:27 -0500

    cleanup

--- src/main/java/frc/robot/constants/HornConstants.java
+++ src/main/java/frc/robot/constants/HornConstants.java
@@ -10,8 +10,6 @@ public class HornConstants { // all values are based on assumptions and lies
   public static final int kHornRightMotorPort = 8;
   public static final int kHornLeftMotorPort = 9;
 
-  public static final double kEncoderTicksPerRevolution = 0;
-
   public static final double kP = 0;
 
   public static final double kI = 0;
