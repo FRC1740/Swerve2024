90f22e5e5cb8184df957b95c186918477cd32530
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-07 15:54:00 -0500

    Current limit, whoops

--- src/main/java/frc/robot/constants/SubsystemConstants/DeflectorConstants.java
+++ src/main/java/frc/robot/constants/SubsystemConstants/DeflectorConstants.java
@@ -2,4 +2,6 @@ package frc.robot.constants.SubsystemConstants;
 
 public class DeflectorConstants {
   public static double kAmpScoringPosition = 0.1; // in 0-1 encoder
+  
+  public static int kDeflectorMotorCurrentLimit = 20;
 }
