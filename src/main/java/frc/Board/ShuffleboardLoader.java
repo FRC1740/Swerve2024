9c073c0ca1d684c0d53ad29841fe2cc1e9ada221
Nikolas Daynard <Nkdaynard@gmail.com>
2024-02-11 17:51:15 -0500

    Testing some shuffleboard auto generation stuff

---
+++ src/main/java/frc/Board/ShuffleboardLoader.java
@@ -0,0 +1,11 @@
+// This loads the .sb code into java shuffleboard code.
+// This makes generic types way easier to create especially for hacking something together.
+// This is just a test
+package frc.Board;
+import java.io.FileReader;
+
+public class ShuffleboardLoader {
+    String sourceDirectory = "src/main/java/frc/Board/SourceSB/";
+    String outputDirectory = "src/main/java/frc/Board/OutputSB/";
+
+}
