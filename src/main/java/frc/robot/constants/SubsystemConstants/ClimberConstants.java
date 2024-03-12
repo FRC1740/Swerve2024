d139e2e7269c7f4d3eaa343f5580298db5ce5104
Nikolas Daynard <Nkdaynard@gmail.com>
2024-03-12 15:44:48 -0400

    new climber softlimits

--- src/main/java/frc/robot/constants/SubsystemConstants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/SubsystemConstants/ClimberConstants.java
@@ -2,6 +2,6 @@
 package frc.robot.constants.SubsystemConstants;
 
 public class ClimberConstants {
-  public static float kClimberMotorForwardSoftLimit = 0.85f;
-  public static float kClimberMotorReverseSoftLimit = -3.05f;
+  public static float kClimberMotorForwardSoftLimit = 1.32f;
+  public static float kClimberMotorReverseSoftLimit = -2.43f;
 }
